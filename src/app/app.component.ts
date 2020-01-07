2a5f115bbf7d4bb4db9b1da2fdc0e0423c6f9e1e
hungtran <hung.tran@bbv.vn>
2020-01-07 13:22:03 +0700

    hid the app component on pages other than the singleton page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from './app.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +9,24 @@ import { AppService } from './app.service';
   providers: []
 })
 export class AppComponent implements OnInit {
-  title = 'ng-adp';
   greeting = '';
+  hideAppComp = false;
 
-  constructor(private appService: AppService) {}
+  constructor(private appService: AppService, private router: Router) {}
 
   ngOnInit() {
     this.appService
       .getGreeting()
       .subscribe(greeting => (this.greeting = greeting));
+
+    this.listenToRouterEvts();
+  }
+
+  private listenToRouterEvts() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.hideAppComp = event.url.search('singleton') === -1;
+      }
+    });
   }
 }
