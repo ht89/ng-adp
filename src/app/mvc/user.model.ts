94294ddfb5935dfb73c8be5f6a32ef350b195ae1
hungtran <hung.tran@bbv.vn>
2020-01-07 16:00:15 +0700

    updated the user model

--- src/app/mvc/user.model.ts
+++ src/app/mvc/user.model.ts
@@ -3,12 +3,18 @@ import { UserService } from './user.service';
 
 export class UserModel {
   user: User;
+  // variable for the UI
+  loading = false;
 
   constructor(private api: UserService) {}
 
   signIn(email: string, password: string) {
+    this.loading = true;
+
     this.api.getUser(email, password).then(user => {
       this.user = user;
+
+      this.loading = false;
     });
   }
 }
