1142698c962e2c9397b33e1e043c3a26eb8cb6ef
hungtran <hung.tran@bbv.vn>
2020-01-07 16:09:18 +0700

    made the form work

--- src/app/mvc/mvc.component.ts
+++ src/app/mvc/mvc.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserModel } from './user.model';
 import { UserService } from './user.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-mvc',
@@ -9,8 +10,8 @@ import { UserService } from './user.service';
 })
 export class MvcComponent implements OnInit {
   private model: UserModel;
-  email = '';
-  password = '';
+  emailCtrl: FormControl = new FormControl();
+  passCtrl: FormControl = new FormControl();
 
   constructor(api: UserService) {
     this.model = new UserModel(api);
@@ -19,6 +20,6 @@ export class MvcComponent implements OnInit {
   ngOnInit() {}
 
   signInClick() {
-    this.model.signIn(this.email, this.password);
+    this.model.signIn(this.emailCtrl.value, this.passCtrl.value);
   }
 }
--- src/app/mvc/mvc.module.ts
+++ src/app/mvc/mvc.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MvcComponent } from './mvc.component';
 import { ROUTES } from './mvc.routes';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [MvcComponent],
-  imports: [ROUTES, CommonModule, FormsModule],
+  imports: [ROUTES, CommonModule, FormsModule, ReactiveFormsModule],
   exports: [MvcComponent]
 })
 export class MvcModule {}
